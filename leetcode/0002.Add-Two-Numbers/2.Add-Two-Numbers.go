d719c815bf6afe46aef2c13c50c5c215de3a51ce
wangzengrui <wangzengrui@darkotino.com>
2020-08-27 14:31:08 +0800

    2.Add-Two-Numbers fix

--- leetcode/0002.Add-Two-Numbers/2.Add-Two-Numbers.go
+++ leetcode/0002.Add-Two-Numbers/2.Add-Two-Numbers.go
@@ -5,24 +5,21 @@ import "LeetCode-Go/structures"
 type ListNode = structures.ListNode
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	head := &ListNode{Val: 0}
-	n1, n2, carry, current := 0, 0, 0, head
-	for l1 != nil || l2 != nil || carry != 0 {
-		if l1 == nil {
-			n1 = 0
-		} else {
-			n1 = l1.Val
+	head := new(ListNode)
+	carry, current := 0, head
+	for l1 != nil || l2 != nil || carry > 0 {
+		current.Next = new(ListNode)
+		current = current.Next
+		if l1 != nil {
+			carry += l1.Val
 			l1 = l1.Next
 		}
-		if l2 == nil {
-			n2 = 0
-		} else {
-			n2 = l2.Val
+		if l2 != nil {
+			carry += l2.Val
 			l2 = l2.Next
 		}
-		current.Next = &ListNode{Val: (n1 + n2 + carry) % 10}
-		current = current.Next
-		carry = (n1 + n2 + carry) % 10
+		current.Val = carry % 10
+		carry /= 10
 	}
 	return head.Next
 }
